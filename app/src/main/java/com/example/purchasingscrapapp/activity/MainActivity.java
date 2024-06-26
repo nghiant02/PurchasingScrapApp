6e9edea63d3c3e0ea598725d2c14ef9beed5f866
Nguyễn Trọng Nghĩa <nghia2852002@gmail.com>
2024-06-27 00:18:21 +0700

    add some field for main

--- app/src/main/java/com/example/purchasingscrapapp/activity/MainActivity.java
+++ app/src/main/java/com/example/purchasingscrapapp/activity/MainActivity.java
@@ -2,22 +2,39 @@ package com.example.purchasingscrapapp.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.purchasingscrapapp.R;
-import com.example.purchasingscrapapp.utils.FirebaseUtils;
-import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button buttonScrapList, buttonSearchScrap, buttonPostScrap, buttonChat, buttonEvaluateScrap, buttonTrackScrap, buttonAnalyzeData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Set up the main activity as needed
+        buttonScrapList = findViewById(R.id.buttonScrapList);
+        buttonSearchScrap = findViewById(R.id.buttonSearchScrap);
+        buttonPostScrap = findViewById(R.id.buttonPostScrap);
+        buttonChat = findViewById(R.id.buttonChat);
+        buttonEvaluateScrap = findViewById(R.id.buttonEvaluateScrap);
+        buttonTrackScrap = findViewById(R.id.buttonTrackScrap);
+        buttonAnalyzeData = findViewById(R.id.buttonAnalyzeData);
+
+        buttonScrapList.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, ScrapListActivity.class)));
+
+//        buttonSearchScrap.setOnClickListener(v -> startActivity(new Intent(MainMenuActivity.this, SearchScrapActivity.class)));
+//
+//        buttonPostScrap.setOnClickListener(v -> startActivity(new Intent(MainMenuActivity.this, PostScrapActivity.class)));
+//
+//        buttonChat.setOnClickListener(v -> startActivity(new Intent(MainMenuActivity.this, ChatActivity.class)));
+//
+//        buttonEvaluateScrap.setOnClickListener(v -> startActivity(new Intent(MainMenuActivity.this, EvaluateScrapActivity.class)));
+//
+//        buttonTrackScrap.setOnClickListener(v -> startActivity(new Intent(MainMenuActivity.this, TrackScrapActivity.class)));
+//
+//        buttonAnalyzeData.setOnClickListener(v -> startActivity(new Intent(MainMenuActivity.this, AnalyzeDataActivity.class)));
     }
 }
