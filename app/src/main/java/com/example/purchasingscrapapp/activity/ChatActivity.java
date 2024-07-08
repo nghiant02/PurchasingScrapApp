1d974e93a9c525c0c676bc154b130be7ec05dabc
Nguyễn Trọng Nghĩa <nghia2852002@gmail.com>
2024-07-08 14:10:00 +0700

    change

---
+++ app/src/main/java/com/example/purchasingscrapapp/activity/ChatActivity.java
@@ -0,0 +1,6 @@
+package com.example.purchasingscrapapp.activity;
+
+import android.app.Activity;
+
+public class ChatActivity extends Activity {
+}
---
+++ app/src/main/java/com/example/purchasingscrapapp/activity/RateOrdersActivity.java
@@ -0,0 +1,6 @@
+package com.example.purchasingscrapapp.activity;
+
+import android.app.Activity;
+
+public class RateOrdersActivity extends Activity {
+}
