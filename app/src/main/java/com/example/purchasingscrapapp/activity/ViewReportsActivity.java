c67bbc0ff98808daf293ef08d956bd129fcd8790
Nguyễn Trọng Nghĩa <nghia2852002@gmail.com>
2024-07-15 19:17:17 +0700

    fix

--- app/src/main/java/com/example/purchasingscrapapp/activity/ViewReportsActivity.java
+++ app/src/main/java/com/example/purchasingscrapapp/activity/ViewReportsActivity.java
@@ -1,4 +1,14 @@
 package com.example.purchasingscrapapp.activity;
 
-public class ViewReportsActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.purchasingscrapapp.R;
+
+public class ViewReportsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_reports);
+    }
 }
